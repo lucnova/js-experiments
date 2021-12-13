0d9d3c920e38ba5be2dbb06852a41d2cc2d125c0
lucnova <nlnovae@gmail.com>
2021-12-12 19:18:04 -0600

    Getters & Setters

--- 07 - Classes/classes.js
+++ 07 - Classes/classes.js
@@ -3,16 +3,42 @@
 // * Declarando una clase
 class Person {
     // * Atributos
-    name = ''; 
-    age = 0;   
+    name = '';
+    age = 0;
 
     // * Constructor
     constructor(name = '', age = 0) {
-        console.log('*Instanciando Persona*');
+        console.log('* Person Init *');
+        let sanitizedAge = parseFloat(age) || 0;
+        sanitizedAge = sanitizedAge < 0 ? 0 : sanitizedAge;
 
-        this.name = name;
-        this.age = age;
+        this.name = name.trim();
+        this.age = sanitizedAge;
     }
+
+    // * SETTERS
+    // *    Diferencia entre no usar el 'set' o 'get'; se declararían como funciones/metodos.
+    //      Al indicar 'set' o 'get' hace que se comporten como atributos (puedes hacer asignaciones)
+    set setName(newName) {
+        this.name = newName.trim();
+    }
+
+    set setAge(newAge) {
+        let sanitizedAge = parseFloat(newAge) || 0;
+        sanitizedAge = sanitizedAge < 0 ? 0 : sanitizedAge;
+
+        this.age = sanitizedAge;
+    }
+
+    // * GETTERS
+    get getName() {
+        return this.name;
+    }
+
+    get getAge() {
+        return this.age;
+    }
+
 
     // * Métodos
     greet() {
@@ -26,3 +52,16 @@ const cass = new Person('Cassandra', 21);
 
 console.log(cass.greet());
 console.log(alan.greet());
+
+console.log('********');
+
+console.log('Cass has', cass.getAge, 'years old.');
+console.log('Alan has', alan.getAge, 'years old.');
+
+cass.setAge = cass.getAge * 3;
+alan.setAge = alan.getAge * 3;
+
+console.log('********');
+
+console.log('Cass now has', cass.getAge, 'years old.');
+console.log('Alan now has', alan.getAge, 'years old.');
