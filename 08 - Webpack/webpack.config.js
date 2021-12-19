db4b1937e2d031f37e4e3d651b31d9978a15b383
lucnova <nlnovae@gmail.com>
2021-12-19 02:01:50 -0600

    More Webpack Info

--- 08 - Webpack/webpack.config.js
+++ 08 - Webpack/webpack.config.js
@@ -4,28 +4,35 @@
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+// * NOTA: Se debe servir la app en el protocolo http (no el protocolo file)
+
 module.exports = {
     mode: 'development',    // Cambiar el modo de trabajo de webpack
     // * Por defecto se coloca el modo de producción, pero el modo de desarrollo
     // *    retira los comentarios y hace varias optimizaciones.
+
+    output: {
+        clean: true,    // * Limpia la carpeta 'dist' y reconstruye
+    },
+
     module: {   // * Se coloca la config de webpack
         rules: [
             // * Mover el archivo 'index.html' a la carpeta 'dist'
             {
-                test: /\.html$/i, // * Condición que evalua webpack al estar barriendo los archivos
+                test: /\.html$/, // * Condición que evalua webpack al estar barriendo los archivos
                 // * Es un patrón regex; en este caso evalua si es un archivo .html
                 loader: 'html-loader',
                 options: {
-                    //sources: false,     
-                    minimize: false,     // * Minificar HTML
+                    sources: false,     // * Mueve las locaciones de los archivos también
+                    minimize: false,    // * Minificar HTML
                 },
             }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
+        new HtmlWebPackPlugin({     // * Control sobre el HTML
+            title: 'Testing Webpack',
             template: './src/index.html',
-            filename: './index.html'
         })
     ]
 };
