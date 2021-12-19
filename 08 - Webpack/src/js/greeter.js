d6180428e18fc194a5a95a75a43e16775285f023
lucnova <nlnovae@gmail.com>
2021-12-19 02:58:28 -0600

    mini css Comments

--- 08 - Webpack/src/js/greeter.js
+++ 08 - Webpack/src/js/greeter.js
@@ -3,6 +3,12 @@ import '../css/Greeter.css';
 //      y no un 'css'. 
 // * Configuar 'style-loader' y 'css-loader' para usar importacion de estilos
 
+// * Por defecto, estos paquetes importan los estilos en el javascript
+//      (aka. no genera un archivo css en el 'dist')
+
+// * mini-css-extract-plugin solventa el problema para incluir el archivo css
+//      como tal
+
 export const Greeter = (greetWho = 'there') => {
     const greeterDiv = document.createElement('p');
     greeterDiv.textContent = `Hey ${greetWho}!`;
