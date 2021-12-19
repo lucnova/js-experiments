e1c03b1cfd1d6ea776aa36d38eee6d9ffa90684a
lucnova <nlnovae@gmail.com>
2021-12-19 02:34:26 -0600

    Greeter Styles

--- 08 - Webpack/src/js/greeter.js
+++ 08 - Webpack/src/js/greeter.js
@@ -1,3 +1,8 @@
+import '../css/Greeter.css';
+// * Webpack por defecto espera leer un archivo 'js'
+//      y no un 'css'. 
+// * Configuar 'style-loader' y 'css-loader' para usar importacion de estilos
+
 export const Greeter = (greetWho = 'there') => {
     const greeterDiv = document.createElement('p');
     greeterDiv.textContent = `Hey ${greetWho}!`;
