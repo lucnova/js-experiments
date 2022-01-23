785e60285955c623a7423371e59db46a0af5499f
lucnova <nlnovae@gmail.com>
2022-01-23 15:00:03 -0600

    Added an Image on div

--- 08 - Webpack/src/js/greeter.js
+++ 08 - Webpack/src/js/greeter.js
@@ -9,9 +9,23 @@ import '../css/Greeter.css';
 // * mini-css-extract-plugin solventa el problema para incluir el archivo css
 //      como tal
 
+// * IMPORTAR IMAGEN
+import webpackLogo from '../assets/img/webpack-logo.png'
+// * No se puede importar archivos por defecto en webpack, es necesario
+//		otro paquete para cargarlos (file-loader https://v4.webpack.js.org/loaders/file-loader/)
+//	Se tiene que configurar también en el archivo 'webpack.config.js'
+// * Es una manera bonita de obtener una URL como importación.
+
+
 export const Greeter = (greetWho = 'there') => {
     const greeterDiv = document.createElement('p');
     greeterDiv.textContent = `Hey ${greetWho}!`;
+
+	//console.log('webpackLogo', webpackLogo);
+	const webpackImg = document.createElement('img');
+	webpackImg.src = webpackLogo;
+
+	greeterDiv.appendChild(webpackImg);
 
     return greeterDiv;
 }
